21a008ce3ed45270fdaf4400c5351be7825339aa
Deepak Gupta <dg3765856@gmail.com>
2026-07-31 13:42:30 +0530

    Final version before deployment

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from google import genai
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-API_KEY = "API_KEY"
+API_KEY = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=API_KEY)
 
